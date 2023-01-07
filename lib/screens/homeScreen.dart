feb0d2539e34792e409424f499ff600e13775804
Gowtham M <>
2023-01-07 07:10:58 +0530

    inital commit

---
+++ lib/screens/homeScreen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 1;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: const Center(
+          child: Text('\t\t\t\t\tWelcome To\n New Galaxy Gym',
+              style: TextStyle(color: Colors.black, fontSize: 30)),
+          // This trailing comma makes auto-formatting nicer for build methods.
+        ));
+  }
+}
